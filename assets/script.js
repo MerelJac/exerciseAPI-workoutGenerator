a4ce22ec08e0c4db57cccd16dd8950ae24f46d52
Merel Jacobs <merel.burleigh@gmail.com>
2023-06-15 12:32:49 -0700

    working API to fetch name and URL

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var submitBtn = document.querySelector('input[type="submit"]');
 var upperChoice = document.querySelector('input[value="upper"]');
 var lowerChoice = document.querySelector('input[value="lower"]');
 var fullbodyChoice = document.querySelector('input[value="full"]');
-
+var upperBodyExercises = [];
 
 submitBtn.addEventListener("click", function(event) {
     // remove prevent default when page is working
@@ -16,3 +16,36 @@ submitBtn.addEventListener("click", function(event) {
     } else {location.reload()}
 })
 
+// fetch("https://exercisedb.p.rapidapi.com/exercises/bodyPartList")
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(error => console.log(error))
+
+async function fetchData() {
+    const url = 'https://exercisedb.p.rapidapi.com/exercises';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '708af97118msh3f74fb80ad20ee2p1f232ejsncd6d167e1b72',
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        // console.log(result[0].gifUrl);
+        for (var i = 0; i < result.length; i++) {
+            if (result[i].bodyPart === 'back' || result[i].bodyPart === 'chest') {
+                upperBodyExercises.push({name: result[i].name, link: result[i].gifUrl})
+            }
+        } console.log(upperBodyExercises)
+    } catch (error) {
+        console.error(error);
+    }};
+
+    fetchData();
